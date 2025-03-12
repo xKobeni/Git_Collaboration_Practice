732e1d72941a912a5745f8b5ee85a6a9fc292646
KngCd <panaligankingcedrick@gmail.com>
2025-03-12 22:01:22 +0800

    add some codes

--- denverSpace/denver.js
+++ denverSpace/denver.js
@@ -7,6 +7,7 @@ DOM.insertAdjacentHTML('beforeend', `
     <div class="vh-100 d-flex flex-column justify-content-center align-items-center text-center bg-light"> 
         <h1>Denver Section</h1>
         <p>Feel free to customize this section.😀</p>
+        <p>SUPOT SI DENVER</p>
     </div>
 
 `);
