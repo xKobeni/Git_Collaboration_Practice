fd9b66bff090ff0283392ff6a532f20e1be35f5c
Lourenz <lourenzfrancisco16@gmail.com>
2025-03-12 23:28:07 +0800

    change color na boss jazz pabali

--- lourenzSpace/lourenz.js
+++ lourenzSpace/lourenz.js
@@ -4,7 +4,7 @@ DOM.insertAdjacentHTML('beforeend', `
 
     <!-- Kayo na bahala kung ano ilalagay nyo dine HAHAHHH -->
 
-    <div class="vh-100 d-flex flex-column justify-content-center align-items-center text-center bg-danger"> 
+    <div class="vh-100 d-flex flex-column justify-content-center align-items-center text-center bg-warning"> 
         <h1>Lourenz Section</h1>
         <p>Pahawak ng iyong BUTU.😀</p>
     </div>
