7ea0212505c91384d02683cf73d057f527f495ca
Lourenz <lourenzfrancisco16@gmail.com>
2025-03-22 23:00:31 +0800

    wkwkw

--- lourenzSpace/lourenz.js
+++ lourenzSpace/lourenz.js
@@ -2,11 +2,12 @@ DOM.insertAdjacentHTML('beforeend', `
 
     <!-- Lourenz Space -->
 
-    <!-- bahala ma si batman perds-->
+    <!-- bahala na si batman perds-->
 
     <div class="vh-100 d-flex flex-column justify-content-center align-items-center text-center bg-light"> 
         <h1>I like trains/h1>
         <p>I was born into a nation of despair, and I alone am the remedy</p>
     </div>
+    <p>hello perds</p>
 
 `);
